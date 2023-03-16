e01e93d669ddde6a0d2ec1e281f2bed0e4890988
Vamshikiran Morlawar <vamshikiranmorlawar@Vamshikirans-MacBook-Air.local>
2023-03-16 14:07:20 +0530

    connected to mysql database successfully

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const mysql = require("mysql");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const db = mysql.createConnection({
-    user: "localhost",
     host: "localhost",
+    user: "root",
     password: "",
     database: "reconchain"
 });
+
+app.get('/', (req, res) => {
+    const sqlInsert = "INSERT INTO user_account (name, email, password, confirmPassword) VALUES ('Deepak', 'deepak09@gmail.com', '12345678', '12345678')";
+    db.query(sqlInsert, (err, result) => {
+        res.send("Sql insert query fired")
+    })
+});
+
+
 
 app.listen(3001, ()=>{
     console.log("running express server");
