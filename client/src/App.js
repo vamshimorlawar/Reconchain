c195ba60c206aeee241a562d263def1156f99b44
Vamshikiran Morlawar <vamshikiranmorlawar@Vamshikirans-MacBook-Air.local>
2023-03-20 21:35:28 +0530

    home text on app component changed

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return(
     <div>
-      <div>Home</div>
+      <div>App</div>
       <Link to="/signup">Sign Up</Link>
       <Link to="/login">Login</Link>
     </div>
