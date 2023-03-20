3fcc769a4624d128d9bd1be822b17cb2d8a1e049
vinay agrawal <vinayagrawalbhilai29@gmail.com>
2023-03-20 19:01:41 +0530

    connected signup component to database

---
+++ reconchain.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 16, 2023 at 09:42 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reconchain`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_account`
+--
+
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `confirmPassword` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_account`
+--
+
+INSERT INTO `user_account` (`id`, `name`, `email`, `password`, `confirmPassword`) VALUES
+(1, 'Deepak', 'deepak09@gmail.com', '12345678', '12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_account`
+--
+ALTER TABLE `user_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server/index.js
+++ server/index.js
@@ -9,33 +9,38 @@ app.use(cors());
 // res.header( "Access-Control-Allow-Origin" );
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "reconchain"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "reconchain",
 });
 
-app.get('/', (req, res) => {
-    console.log("Basic Api");
+app.get("/", (req, res) => {
+  console.log("Basic Api");
 });
 
-app.post('/signup', (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
 
-    if(password == confirmPassword)
-    {
-        res.send({status: "Success"})
-    }
-    else{
-        res.send({status: "Failure"})
-    }
-})
+  if (password == confirmPassword) {
+    const query =
+      "INSERT INTO user_account (name, email, password) VALUES (?,?,?)";
+    db.query(
+      query,
+      [username, email, password, confirmPassword],
+      (err, result) => {
+        console.log(err, result);
+        res.send({ status: "Success" });
+      }
+    );
+  } else {
+    res.send({ status: "Failure" });
+  }
+});
 
-
-
-app.listen(3001, ()=>{
-    console.log("running express server");
+app.listen(3001, () => {
+  console.log("running express server");
 });
