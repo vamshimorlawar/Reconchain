9e1c52c794c1e3585ab1fca8ec4a7870870ba3d8
Vamshikiran Morlawar <vamshikiranmorlawar@Vamshikirans-MacBook-Air.local>
2023-04-15 20:37:15 +0530

    creat job application contract integrated

--- client/src/components/CandidateJobApplication/CandidateJobApplication.js
+++ client/src/components/CandidateJobApplication/CandidateJobApplication.js
@@ -5,13 +5,21 @@ import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import CandidateNav from "../CandidateNav/CandidateNav";
 import { useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import { ethers } from "ethers";
+import Reconchain from "../../artificats/contracts/Reconchain.sol/Reconchain.json";
+// The contract address
+const reconchainAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+var eventBlocks = new Set();
+const regex = /'([^']+)'/;
 
 const CandidateJobApplication = () => {
   const id = useParams().id;
   const company_email = useParams().company_email;
 
   const [jobData, setJobData] = useState(null);
- 
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -29,29 +37,74 @@ const CandidateJobApplication = () => {
       });
   };
 
+  async function createJobApplication() {
+    async function requestAccount() {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    }
 
-  const createApplication = (event) => {
-    axios.post("http://localhost:3001/createApplicationProfile",{
-      candidate_email: sessionStorage.getItem("email"),
-      company_email: jobData.company_email,
-      job_id: jobData.id      
-    }).then((res) => {
-      if (res.data.status === "success") {
-        console.log("dProfile updated");
-      } else {
-        console.log("Update Failed");
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(
+        reconchainAddress,
+        Reconchain.abi,
+        signer
+      );
+      try {
+        console.log("Formadata block job id", jobData.block_job_id);
+        await contract.createJobApplication(
+          jobData.block_job_id,
+          jobData.address
+        );
+        contract.on(
+          "JobApplicationCreated",
+          function (address, block_job_id, company_address, event) {
+            let blockNumber = event.blockNumber;
+            if (eventBlocks.has(blockNumber)) return;
+            eventBlocks.add(blockNumber);
+            console.log("Address return - ",address);
+            if (address) {
+              axios
+                .post("http://localhost:3001/createApplicationProfile", {
+                  candidate_email: sessionStorage.getItem("email"),
+                  company_email: jobData.company_email,
+                  job_id: jobData.id,
+                })
+                .then((res) => {
+                  if (res.data.status === "success") {
+                    console.log("dProfile updated");
+                    toast.success("Application Submitted");
+                  } else {
+                    console.log("Update Failed");
+                    toast.error("Application Submission Failed")
+                  }
+                });
+            }
+          }
+        );
+      } catch (error) {
+        toast.error(regex.exec(error.reason)[1]);
+        console.log("Error is ", regex.exec(error.reason)[1]);
       }
-    });
+    }
+  };
 
+  const createApplication = async (event) => {
+    event.preventDefault();
+    await createJobApplication();
   };
 
   if (!jobData) {
     return <div>Loading ...</div>;
-  }
+  };
 
   return (
     <div>
       <CandidateNav></CandidateNav>
+      <ToastContainer/>
       <Container className="my-4">
         <Row className="justify-content-center">
           <Col xs={12} md={8} lg={6}>
